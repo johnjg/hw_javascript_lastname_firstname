b01e0cae979bc878bcfdcd5fde1ac5c24d3ad2d6
John Gotschall <contact@johnjg.com>
2016-06-11 00:26:06 -0400

    add the presidents object script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
 //arrays branch
 var iceCream = ['vanilla', 'chocolate', 'strawberry'];
 iceCream.push('mint');
+//presidents branch
+var presidents = [
+  {
+    firstName: 'Barrack',
+    lastName: 'Obama',
+    termLength: 8,
+    party: 'Democrat',
+    yearsOfPresidency: '2009-2016'
+  },
+  {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 8,
+    party: 'Republican',
+    yearsOfPresidency: '2001-2009'
+  },
+  {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: 8,
+    party: 'Democrat',
+    yearsOfPresidency: '1993-2001'
+  },
+  {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 4,
+    party: 'Democrat',
+    yearsOfPresidency: '1989-1993'
+  },
+  {
+    firstName: 'Ronald',
+    lastName: 'Reagan',
+    termLength: 8,
+    party: 'Democrat',
+    yearsOfPresidency: '1981-1989'
+  }
+];
+console.log ( presidents[2].firstName + ' ' + presidents[2].lastName );
